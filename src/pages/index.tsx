7d296d05f95246ddab1798d77d7f81a682f3f1fc
Lucas_Orleans <lucasorleans.pro@gmail.com>
2025-12-18 08:21:14 -0300

    Adiciona a visualização de porta selecionada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,10 @@ import Porta from "../components/Porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
-  const [p2, setP2] = useState(new PortaModel(2))
 
   return (
     <div style={{display: "flex"}}>
-      <Porta porta={p1}/>
-      <Porta porta={p2}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   );
 }
