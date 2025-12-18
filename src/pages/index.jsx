85a9aef3965acddce060f9c1f00c6d3ffb7326f9
Lucas_Orleans <lucasorleans.pro@gmail.com>
2025-12-18 07:52:20 -0300

    Adiciona estrutura visual das portas, a lógica de seleção. Adiciona Typescript ao projeto.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import PortaModel from "../model/PortaModel";
 import Presente from "../components/Presente";
-import Porta from "@/components/Porta";
+import Porta from "../components/Porta";
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+  const [p2, setP2] = useState(new PortaModel(2))
+
   return (
-    <div>
-      <Porta />
+    <div style={{display: "flex"}}>
+      <Porta porta={p1}/>
+      <Porta porta={p2}/>
     </div>
   );
 }
